22ed62ca220f84f10e29bd43c0fdac138234c132
KrashMunoz <krash.munoz@gmail.com>
2019-11-20 14:40:02 -0500

    added httpclient service

--- src/app/calculate.service.ts
+++ src/app/calculate.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CalcForm } from './calc-form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculateService {
 
-  constructor() { }
+  _url = '';
+  constructor(private _http: HttpClient) { }
+
+  calculate(calcForm: CalcForm) {
+    this._http.post<any>(this._url, calcForm);
+  }
 }
