b78b43dc2bc6e2690d849a6a0786207f6d438959
KrashMunoz <krash.munoz@gmail.com>
2019-11-22 00:17:03 -0500

    angular material module 2

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDividerModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatIconModule
+]
+
+@NgModule({
+  declarations: [MaterialComponents],
+  imports: [MaterialComponents]
+})
+export class MaterialModule { }
